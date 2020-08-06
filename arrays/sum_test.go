bf17d1a525280a2d23491123e1d889e5919d8acc
Luke Danielson <danielsonlukea@gmail.com>
2020-08-06 15:43:02 -0600

    finished arrays tutorial

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -9,7 +9,12 @@ func assertMessage(t *testing.T, got int, want int, numbers []int) {
 	if got != want {
 		t.Errorf("got %d want %d given: %v", got, want, numbers)
 	}
+}
 
+func assertArraysEqual(t *testing.T, got []int, want []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
 
 func TestSum(t *testing.T) {
@@ -38,8 +43,25 @@ func TestSumAll(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertArraysEqual(t, got, want)
+
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("two collections of same size", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		assertArraysEqual(t, got, want)
+
+	})
+
+	t.Run("safely handle empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		assertArraysEqual(t, got, want)
+
 	})
 }
